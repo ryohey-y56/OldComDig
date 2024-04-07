e6d84eedcc81768735069133170d65a2c5483331
forest741 <mont.blanc.jan@gmail.com>
2024-04-08 00:20:30 +0900

    upd アプリ専用のアルバムの作成、保存

--- lib/camera.dart
+++ lib/camera.dart
@@ -5,6 +5,11 @@ import 'package:flutter/material.dart';
 import 'package:camera/camera.dart';
 import 'package:image_gallery_saver/image_gallery_saver.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:external_path/external_path.dart';
+import 'package:photo_manager/photo_manager.dart';
+import 'package:intl/intl.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:collection/collection.dart';
 
 Future<void> main() async {
   // main 関数内で非同期処理を呼び出すための設定
@@ -98,6 +103,7 @@ class TakePictureScreenState extends State<TakePictureScreen> {
           //mainAxisSize: MainAxisSize.min,
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
+            //画像を開く
             FloatingActionButton(
               onPressed: () {
                 final picker = ImagePicker();
@@ -105,10 +111,12 @@ class TakePictureScreenState extends State<TakePictureScreen> {
               },
               child: const Icon(Icons.image),
             ),
+            //写真を撮る
             FloatingActionButton(
               onPressed: () async {
-                //写真を撮る
                 final image = await _controller.takePicture();
+                final file = File(image.path);
+                saveimage(file);
                 final Uint8List buffer = await image.readAsBytes();
                 await ImageGallerySaver.saveImage(buffer, name: image.name);
                 //表示用の画面に遷移
@@ -125,6 +133,63 @@ class TakePictureScreenState extends State<TakePictureScreen> {
           ],
         ));
   }
+}
+
+//画像の保存
+Future<void> saveimage(File file) async {
+  final tempDir = await getTemporaryDirectory();
+  final albumName = 'oldComDig';
+
+  // create save directory
+  if (Platform.isAndroid) {
+    final picturesPath = await ExternalPath.getExternalStoragePublicDirectory(
+      ExternalPath.DIRECTORY_PICTURES,
+    );
+    final albumPath = '$picturesPath/$albumName';
+    print(albumPath);
+    // If directory does not exist, create directory before writing.
+    await Directory(albumPath).create(recursive: true);
+  }
+
+  final now = DateTime.now();
+  DateFormat outputFormat = DateFormat('yyyy-MM-dd_HH-mm_ssS');
+  final fileName = '${outputFormat.format(now)}.jpg';
+  print('file name: $fileName');
+
+  final uint8list = file.readAsBytesSync();
+  final List<int> fileByte = uint8list;
+  file = File('${tempDir.path}/$fileName')
+        ..writeAsBytesSync(fileByte);
+  print('temp file path: ${file.path}');
+  final permissionState = await PhotoManager.requestPermissionExtend();
+  if (!permissionState.isAuth) {
+    debugPrint('Please allow access and try again.');
+    return;
+  }
+
+  // save image
+  final assetEntity = await PhotoManager.editor.saveImageWithPath(
+    file.path,
+    title: fileName,
+    relativePath: Platform.isAndroid ? 'Pictures/$albumName' : albumName,
+  );
+  print('assetEntity: $assetEntity');
+
+  // iOS needs tagging to image.
+  if (Platform.isIOS) {
+    final paths = await PhotoManager.getAssetPathList();
+    var assetPathEntity = paths.firstWhereOrNull((e) => e.name == albumName);
+    // If album does not exist, you also need to use the createAlbum method before copying.
+    assetPathEntity ??= await PhotoManager.editor.darwin.createAlbum(albumName);
+    await PhotoManager.editor.copyAssetToPath(
+      asset: assetEntity!,
+      pathEntity: assetPathEntity!,
+    );
+  }
+
+  // clear cache
+  file.deleteSync(recursive: true);
+  print('completed!');
 }
 
 //撮影した写真を表示する画面
